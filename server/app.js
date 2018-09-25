20a0830cfafea8560cc6a019ac32c5cb96de764d
Ali Naeem <alitheflyingtiger111@gmail.com>
2018-09-25 17:15:15 +0500

    rxjs added

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
 // ./express-server/app.js
-import express from 'express';
-import path from 'path';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import mongoose from 'mongoose';
-import SourceMapSupport from 'source-map-support';
-import bb from 'express-busboy';
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import logger from "morgan";
+import mongoose from "mongoose";
+import SourceMapSupport from "source-map-support";
+import bb from "express-busboy";
+
+// importing ReactiveX
+import Rx from "rxjs";
+import { Subject } from "rxjs/Subject";
+
+var requests_ = new Subject();
+
+function sendHello(e) {
+  console.log("sending hello");
+  e.res.writeHead(200, { "Content-Type": "text/plain" });
+  e.res.end("Hello World\n");
+}
+
+requests_.subscribe(sendHello);
 
 // import routes
-import todoRoutes from './routes/todo.server.route';
+import todoRoutes from "./routes/todo.server.route";
 
 // define our app using express
 const app = express();
@@ -17,46 +31,48 @@ const app = express();
 bb.extend(app);
 
 // allow-cors
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
-})
-
+});
 
 // configure app
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // set the port
 const port = process.env.PORT || 3001;
 
 // connect to database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/UltimateTodo', {
-  useMongoClient: true,
-});
+mongoose.connect(
+  "mongodb://localhost:27017/UltimateTodo",
+  {
+    useMongoClient: true
+  }
+);
 
 // add Source Map Support
 SourceMapSupport.install();
 
-app.use('/api', todoRoutes);
+app.use("/api", todoRoutes);
 
-app.get('/', (req,res) => {
-  return res.end('Api working');
-})
+app.get("/", (req, res) => {
+  return res.end("Api working");
+});
 
 // catch 404
 app.use((req, res, next) => {
-  res.status(404).send('<h2 align=center>Page Not Found!</h2>');
+  res.status(404).send("<h2 align=center>Page Not Found!</h2>");
 });
 
-
-
 // start the server
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`App Server Listening at ${port}`);
 });
--- server/controllers/todo.server.controller.js
+++ server/controllers/todo.server.controller.js
@@ -1,60 +1,147 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import Rx from "rxjs";
+import { map } from "rxjs/operators";
 
 //import models
-import Todo from '../models/todo.server.model';
+import Todo from "../models/todo.server.model";
 
-export const getTodos = (req,res) => {
-  Todo.find().exec((err,todos) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
+export const getTodos = (req, res) => {
+  Todo.find().exec((err, todos) => {
+    if (err) {
+      return res.json({ success: false, message: "Some Error" });
     }
 
-    return res.json({'success':true,'message':'Todos fetched successfully',todos});
+    return res
+      .json({
+        success: true,
+        message: "Todos fetched successfully",
+        todos
+      })
+      .pipe(
+        map(res => {
+          if (res) {
+            return true;
+          }
+          return false;
+        })
+      );
   });
-}
+};
 
-export const addTodo = (req,res) => {
+export const addTodo = (req, res) => {
   console.log(req.body);
   const newTodo = new Todo(req.body);
-  newTodo.save((err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
+  newTodo.save((err, todo) => {
+    if (err) {
+      return res.json({ success: false, message: "Some Error" });
     }
 
-    return res.json({'success':true,'message':'Todo added successfully',todo});
-  })
-}
+    return res
+      .json({
+        success: true,
+        message: "Todo added successfully",
+        todo
+      })
+      .pipe(
+        map(res => {
+          if (res) {
+            return true;
+          }
+          return false;
+        })
+      );
+  });
+};
 
-export const updateTodo = (req,res) => {
-  Todo.findOneAndUpdate({ _id:req.body.id }, req.body, { new:true }, (err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error','error':err});
+export const updateTodo = (req, res) => {
+  Todo.findOneAndUpdate(
+    { _id: req.body.id },
+    req.body,
+    { new: true },
+    (err, todo) => {
+      if (err) {
+        return res
+          .json({ success: false, message: "Some Error", error: err })
+          .pipe(
+            map(res => {
+              if (res) {
+                return true;
+              }
+              return false;
+            })
+          );
+      }
+      console.log(todo);
+      return res
+        .json({ success: true, message: "Updated successfully", todo })
+        .pipe(
+          map(res => {
+            if (res) {
+              return true;
+            }
+            return false;
+          })
+        );
     }
-    console.log(todo);
-    return res.json({'success':true,'message':'Updated successfully',todo});
-  })
-}
+  );
+};
 
-export const getTodo = (req,res) => {
-  Todo.find({_id:req.params.id}).exec((err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
+export const getTodo = (req, res) => {
+  Todo.find({ _id: req.params.id }).exec((err, todo) => {
+    if (err) {
+      return res.json({ success: false, message: "Some Error" });
     }
-    if(todo.length){
-      return res.json({'success':true,'message':'Todo fetched by id successfully',todo});
+    if (todo.length) {
+      return res
+        .json({
+          success: true,
+          message: "Todo fetched by id successfully",
+          todo
+        })
+        .pipe(
+          map(res => {
+            if (res) {
+              return true;
+            }
+            return false;
+          })
+        );
+    } else {
+      return res
+        .json({
+          success: false,
+          message: "Todo with the given id not found"
+        })
+        .pipe(
+          map(res => {
+            if (res) {
+              return true;
+            }
+            return false;
+          })
+        );
     }
-    else{
-      return res.json({'success':false,'message':'Todo with the given id not found'});
-    }
-  })
-}
+  });
+};
 
-export const deleteTodo = (req,res) => {
-  Todo.findByIdAndRemove(req.params.id, (err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
+export const deleteTodo = (req, res) => {
+  Todo.findByIdAndRemove(req.params.id, (err, todo) => {
+    if (err) {
+      return res.json({ success: false, message: "Some Error" });
     }
 
-    return res.json({'success':true,'message':todo.todoText+' deleted successfully'});
-  })
-}
+    return res
+      .json({
+        success: true,
+        message: todo.todoText + " deleted successfully"
+      })
+      .pipe(
+        map(res => {
+          if (res) {
+            return true;
+          }
+          return false;
+        })
+      );
+  });
+};
